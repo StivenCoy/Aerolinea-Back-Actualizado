7737f24c48ecb9767e71479d8b9ef1b8c5807641
EstibenCoy <stivenrodriguez1270@gmail.com>
2022-04-01 10:10:48 -0500

    modificacion en el direccion

--- src/main/java/com/aerolinea/app/AerolineaApplication.java
+++ src/main/java/com/aerolinea/app/AerolineaApplication.java
@@ -19,7 +19,7 @@ public class AerolineaApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://aerolinea-bebold-backend.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://aerolinea-bebold-backend.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
